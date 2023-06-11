3c79063597550c16199dab0d1f764469a9a2a962
Allen Gustrowsky <allen@may21.us>
2023-06-11 16:49:07 -0400

    added divide()

--- add.py
+++ add.py
@@ -1,5 +1,13 @@
+from typing import Union
+
 def add(nums: list) -> float:
     return sum(nums)
     
 def multiply(a: float, b: float) -> float:
     return a * b
+
+def divide(a: float, b: float) -> Union[float, str]:
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Division by zero not allowed." 
