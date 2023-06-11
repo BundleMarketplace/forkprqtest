a6063f15b22078a520ca12a4d06b7de766d69901
Allen Gustrowsky <65049787+allengustrowsky@users.noreply.github.com>
2023-06-11 16:59:20 -0400

    Feature testing this (#4)
    * test commit
    
    * add function
    
    * added multiply(), modified add() type
    
    * added divide()
    
    * added mystery function
    
    ---------
    
    Co-authored-by: BundleMarketplace <118932414+BundleMarketplace@users.noreply.github.com>

--- add.py
+++ add.py
@@ -1,5 +1,16 @@
+from typing import Union
+
 def add(nums: list) -> float:
     return sum(nums)
     
 def multiply(a: float, b: float) -> float:
     return a * b
+
+def divide(a: float, b: float) -> Union[float, str]:
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return "Division by zero not allowed." 
+
+def just_because():
+    print("bet you weren't expecting this turn of events!")
